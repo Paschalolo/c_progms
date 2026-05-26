1e1179db99a6d7594a3a42d23b8b6f2c7a90d68c
Paschalolo <pastechnology1@gmail.com>
2026-05-26 11:37:03 -0500

    mesas

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
+#define __STDC_LIB_EXT1__
 int main(){
-	printf("Hello world");
+	char name[256] = {0};
+
+	printf("Enter name : ");
+	[[maybe_unused]]int a = scanf("%s", name);
+	printf("%s", name);
 	return 0;
 }
