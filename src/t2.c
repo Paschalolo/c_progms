d559b25daa2e3c159ba65676d9b3df2f6da227e5
Paschalolo <pastechnology1@gmail.com>
2026-05-26 14:49:06 -0500

    mesas

--- src/t2.c
+++ src/t2.c
@@ -2,10 +2,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
 int main(){
-	char name[256] = {0};
-	int a =gethostname(name , 256);
-	if(a == -1 )exit(1);
+	char name[2] = {0};
+	int a =gethostname(name , 2);
+	if(a == -1 ){
+		switch(errno){
+			case EFAULT : 
+				printf("Name is invalid");
+				break;	
+			case ENAMETOOLONG:
+				printf("Name is truncated and too long lost some value");
+				break; 
+			case EINVAL : 
+				printf("size is negative than the actual size");
+				break;
+		}
+		exit(1);
+	}
 	printf("%s", name);
 	return 0;
 }
