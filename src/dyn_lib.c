954c52a5a1bfb5391ea20cbc656ad29a37d1477b
Paschalolo <pastechnology1@gmail.com>
2026-06-15 18:08:45 -0500

    message

--- src/dyn_lib.c
+++ src/dyn_lib.c
@@ -6,5 +6,24 @@ int main([[maybe_unused]] int argc , [[maybe_unused]] char** argv){
 		fprintf(stderr, "error no file");
 		exit(1);
 	}
+	printf("%s %s", argv[1] , argv[2]);
+	int res ; 
+	res = load_libary(argv[1]);
+	if(res != 0){
+		fprintf(stderr, "couldnt load libary");
+		exit(1);
+	}
+	typedef union func_t {
+		void* ptr ; 
+		void (*func1)(int); 
+	}func_t;
+	func_t yh ={ NULL}; 
+
+	yh.ptr = pdy_symbol(argv[2]); 
+	yh.func1(88);
+
+	close_libary();
+	
+	printf("finito");
 }
 
