3c4ebc30b9b371fd4dfafc7f60f4c5c95917314b
Paschalolo <pastechnology1@gmail.com>
2026-06-19 18:36:42 -0500

    message

---
+++ src/utime.c
@@ -0,0 +1,18 @@
+
+
+/*
+ *	Change access and modification times of a file (setting it backwards) 
+ *	Copyright (c) By Mr Paschal Ahanmisi
+ * */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <utime.h>
+
+
+int main(){}
