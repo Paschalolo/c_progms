913eddf51c6cf297b652ae70dd8ebaf3b6d4f282
Paschalolo <pastechnology1@gmail.com>
2026-07-01 15:14:11 -0500

    message

---
+++ includes/mysock.h
@@ -0,0 +1,16 @@
+#ifndef MY_SOC 
+#define MY_SOC
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+#endif
---
+++ src/udpclt_conn_all.c
@@ -0,0 +1,7 @@
+
+/* A connectionless client program using conncet with datagrams sockets 
+ *	Usage >> 
+ *	Authored by Mr Paschal Ahanmisi
+ * */
+
+#include 
