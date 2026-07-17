0b6ebb4e1e9f4cab35cae4b82502ca34768521ad
Paschalolo <pastechnology1@gmail.com>
2026-07-17 12:59:17 -0500

    message

---
+++ src/rlimti.c
@@ -0,0 +1,17 @@
+/* Total amount of memoery the process can lock in memeory
+ * This can be called with mlock() or mmap -> MMAP_LOCKED 
+ * to lock the memory into place */
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <stdio.h>
+
+
+
+int main(){
+	struct rlimit rlt ; 
+	int ret ;
+	ret =getrlimit(RLIMIT_MEMLOCK, &rlt );
+	if(ret == -1){return -1;}
+	printf("Locked memroy limit : %ld\n", rlt.rlim_max);
+	return 0;
+}
