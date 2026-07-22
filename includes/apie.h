74e19d3d5725e375f1f7b8a4a4c488c9b9a67f0b
Paschalolo <pastechnology1@gmail.com>
2026-07-22 13:14:52 -0500

    message

--- includes/apie.h
+++ includes/apie.h
@@ -4,6 +4,10 @@
 #define HH_PP_Y 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
 __attribute__((cold, noinline))void err_sys(const char* __restrict__ str){
 	fprintf(stderr , "%s" , str);
 	 exit(1);
