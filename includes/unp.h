6bfda9f4245e9ef38a8cface4d3f66fcf4f2f3ef
Paschalolo <pastechnology1@gmail.com>
2026-08-01 16:19:29 -0500

    message

--- includes/unp.h
+++ includes/unp.h
@@ -1,11 +1,13 @@
 
 #ifndef  UNP_H 
 #define UNP_H 
+#include <cerrno>
 #include <sys/socket.h>
 #include <cstdio>
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 #include <arpa/inet.h>
 #include <cstdlib>
 #include <sys/utsname.h>
@@ -14,7 +16,19 @@
 #include <time.h>
 constexpr size_t MAX_LINE = 1024 ; 
 constexpr size_t BACK_LOG = 4096;
-
+struct My_sockaddr_in_t {
+	My_sockaddr_in_t(sa_family_t  family , in_port_t port  ,in_addr_t addr) noexcept {
+			m_sockaddr.sin_family = family ; 
+			m_sockaddr.sin_port = port ;
+			m_sockaddr.sin_addr.s_addr = addr ; 
+	}
+	My_sockaddr_in_t() = default;
+	My_sockaddr_in_t(const My_sockaddr_in_t&) = delete ; 
+	My_sockaddr_in_t( My_sockaddr_in_t&&) = delete ; 
+	My_sockaddr_in_t& operator=( My_sockaddr_in_t&&) = delete ; 
+	My_sockaddr_in_t& operator=( const My_sockaddr_in_t&) = delete ; 
+	struct sockaddr_in m_sockaddr{} ;
+};
 void err_quit(const char* __restrict__ str ){
 	std::fprintf(stderr , "%s", str);
 	exit(EXIT_FAILURE); 
@@ -32,5 +46,22 @@ __attribute__((always_inline)) static inline int Socket(int family , int type , 
 	}
 	return n;
 }
+
+void str_echo(int fd){
+	ssize_t n ; 
+	char buf[MAX_LINE] ; 
+	while ((n = read(fd , buf  , MAX_LINE)) >= 0 ){
+		if((n == 0) && (errno == EINTR)){
+			continue;
+		}else {
+			return ;
+		}
+		[[maybe_unused]] ssize_t ret = write(fd , buf , static_cast<size_t>(n) )  ; 
+	}
+	if(n == -1){
+		err_sys("read() failed\n");
+	}
+}
+
 #endif 
 
---
+++ src/tcserver.cpp
@@ -0,0 +1,41 @@
+/* This porject by Paschal Ahanmisi */
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unp.h>
+
+
+int main(){
+	int listenfd , connfd ; 
+	pid_t childpid ; 
+	socklen_t chilen {static_cast<socklen_t>(sizeof(struct sockaddr_in))};  
+	My_sockaddr_in_t cliaddr{} , srvadr{AF_INET , htons(8080) ,htonl(INADDR_ANY) };
+	if ((listenfd = socket(AF_INET , SOCK_STREAM , 0)) == -1 ){
+		err_sys("socket()\n");
+	}
+
+	// bind 
+	if(bind(listenfd  ,reinterpret_cast<const sockaddr*>(&srvadr.m_sockaddr), sizeof srvadr ) == -1 ){
+		err_sys("bind () failed");
+	}
+	
+	// lsiuten 
+	if(listen(listenfd , BACK_LOG) == -1 ){
+		err_sys("listen failed");
+	}
+
+	for(;;){
+		connfd = accept(listenfd , reinterpret_cast<struct sockaddr*>(&cliaddr.m_sockaddr) ,&chilen);
+		if(connfd == -1) {
+			std::fprintf(stderr , "Connection failed\n");
+			continue;
+		}
+		if((childpid = fork()) == 0 ){
+
+		}else if (childpid == -1){
+			err_sys("could not fork");
+		}
+	}
+	close(listenfd);
+	return 0;
+}
+
