b85bf92d68d2eaf728b38c834c0ed7d948a2fb1e
Paschalolo <pastechnology1@gmail.com>
2026-08-03 12:47:12 -0500

    message

---
+++ src/getnam.cpp
@@ -0,0 +1,26 @@
+
+
+#include <sys/socket.h>
+#include <unp.h>
+#include <netdb.h>
+#include <sys/types.h>
+
+#include <arpa/inet.h>
+int main(){
+	struct addrinfo hints {}; 
+	struct addrinfo *res ; 
+	hints.ai_family = AF_UNSPEC ; 
+	hints.ai_socktype = 0  ; 
+	if (getaddrinfo("www.google.com", "https", &hints, &res) != 0 ) {
+		err_sys("Failed lookup");
+	}
+	auto r_ptr = res ; 
+	char dst[INET6_ADDRSTRLEN]{0};
+	while(r_ptr != nullptr){
+		std::printf("%s\n" , inet_ntop(r_ptr->ai_family ,reinterpret_cast<void*>(r_ptr->ai_addr) ,dst , r_ptr->ai_addrlen  ));
+	r_ptr = r_ptr->ai_next;
+	}
+
+	freeaddrinfo(res);
+
+}
