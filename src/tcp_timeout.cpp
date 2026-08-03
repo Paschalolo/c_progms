875fb68c8e215f0e51520f924c8420c6ca7043db
Paschalolo <pastechnology1@gmail.com>
2026-08-03 01:09:50 -0500

    message

---
+++ src/tcp_timeout.cpp
@@ -0,0 +1,45 @@
+
+
+#include <cerrno>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <cstdio>
+#include <unistd.h>
+int main(){
+	struct sockaddr_in srvaddr{} ; 
+	int sockfd ; 
+	int connfd ; 
+	if((sockfd = socket(AF_INET , SOCK_STREAM , 0)) == -1 ){
+		return -1;
+	}
+	srvaddr.sin_addr.s_addr = INADDR_ANY ; 
+	srvaddr.sin_port = htons(8080); 
+	srvaddr.sin_family = AF_INET;
+	
+	if(bind(sockfd , reinterpret_cast<const struct sockaddr*>(&srvaddr), sizeof srvaddr)){return -2;}
+	if(listen(sockfd , 1024) == -1 ) {return -3;}
+	int optval = 1 ;
+	int mkeepcnt = 1 ;
+	int m_keepidle = 10 ; 
+	int m_kintp = 10 ; 
+	setsockopt(sockfd ,SOL_SOCKET,SO_KEEPALIVE ,&optval , sizeof optval );
+	setsockopt(sockfd ,IPPROTO_TCP, TCP_KEEPCNT  , &mkeepcnt , sizeof optval );
+	setsockopt(sockfd ,IPPROTO_TCP,TCP_KEEPIDLE ,&m_keepidle , sizeof optval );
+	setsockopt(sockfd ,IPPROTO_TCP,TCP_KEEPINTVL ,&m_kintp , sizeof optval );
+	for(;;){
+	 	connfd =accept(sockfd  , nullptr , nullptr);
+		if(connfd == -1){
+			if(errno ==   ETIMEDOUT){
+				std::fprintf(stderr , "ERRTIMEOUT connection was closed\n");
+				break ;
+			}else {
+				std::fprintf(stderr , "I dont know what it is \n");
+				break ;
+			}
+		}
+		}
+	close(sockfd);
+return 0;	
+}
